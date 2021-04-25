b48c9ba70b824132a33ef36f5e0332eb2d8a627d
JUHYEOK <wngur6076@naver.com>
2021-04-25 23:12:05 +0900

    [#14] 기능: 스터티 참여 취소하기 구현 (#16)
    - 개설자 참여 취소 구현
    - 신청자 참여 취소 구현
    - 유효성 검사
    - 테스트코드 확인
    
    resolves #14

---
+++ tests/Feature/StudyRequestCancelTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\StudyRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StudyRequestCancelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function study_requests_can_be_canceled()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $anotherUser = User::factory()->create();
+        $this->actingAs($anotherUser, 'api')
+            ->post("/api/study-groups/{$post->id}/request", [
+                'reason' => '테스트',
+                'project' => 'test',
+            ])->assertStatus(200);
+
+        $response = $this->actingAs($user, 'api')
+            ->delete("/api/study-groups/{$post->id}/request-response", [
+                'user_id' => $anotherUser->id,
+            ])->assertStatus(204);
+
+        $studyRequest = StudyRequest::first();
+        $this->assertNull($studyRequest);
+        $response->assertNoContent();
+    }
+
+    /** @test */
+    function requestor_can_cancel_a_study_request()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $anotherUser = User::factory()->create();
+        $this->actingAs($anotherUser, 'api')
+            ->post("/api/study-groups/{$post->id}/request", [
+                'reason' => '테스트',
+                'project' => 'test',
+            ])->assertStatus(200);
+
+        $response = $this->actingAs($anotherUser, 'api')
+            ->delete("/api/study-groups/{$post->id}/request-response", [
+                'user_id' => $anotherUser->id,
+            ])->assertStatus(204);
+
+        $studyRequest = StudyRequest::first();
+        $this->assertNull($studyRequest);
+        $response->assertNoContent();
+    }
+
+    /** @test */
+    function user_id_is_required()
+    {
+        $this->actingAs($user = User::factory()->create(), 'api');
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->json('delete', "/api/study-groups/{$post->id}/request-response", [
+                'user_id' => '',
+            ])->assertStatus(422);
+
+        $responseString = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('user_id', $responseString['errors']);
+    }
+}
