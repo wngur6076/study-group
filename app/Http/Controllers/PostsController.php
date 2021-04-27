6dc9dec68a3aa22af26b1fb55fb6ee18cddf8d9a
wngur6076 <wngur6076@naver.com>
2021-04-27 14:46:18 +0900

    [#19] 기능: 스터디정보 수정하기 구현 - 스터디정보 수정하기 구현 - 스터디 태그정보 수정하기 구현 - 테스트코드 통과 확인

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Post as PostResource;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
@@ -38,6 +39,37 @@ class PostsController extends Controller
     public function show($id)
     {
         $post = Post::findOrFail($id);
+
+        return new PostResource($post);
+    }
+
+    public function update($id)
+    {
+        $post = Auth::user()->posts()->findOrFail($id);
+
+        $this->validate(request(), [
+            'title' => ['required'],
+            'body' => ['required'],
+            'zone' => ['required'],
+            'deadline' => ['required'],
+            'max_number_people' => ['required', 'numeric'],
+            'tags' => ['nullable', 'array'],
+        ]);
+
+        $post->update([
+            'title' => request('title'),
+            'body' => request('body'),
+            'zone' => request('zone'),
+            'deadline' => request('deadline'),
+            'max_number_people' => request('max_number_people'),
+        ]);
+
+        if (request('tags')) {
+            $post->tags()->delete();
+            foreach (request('tags') as $tag) {
+                $post->tags()->create(['name' => $tag]);
+            }
+        }
 
         return new PostResource($post);
     }
